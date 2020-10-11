cc0f7e2e3ec3c910759b6851f4f91e073a336b18
Ivan <ivan.ingov@gmail.com>
2020-10-11 23:46:02 +0300

    Updates

---
+++ precache-manifest.8913165df0ab1b9955b5dc1fef11521c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7aced0df61174eb5c720ccd09a4b7508",
+    "url": "/first-project/index.html"
+  },
+  {
+    "revision": "104055a48345a055e6c2",
+    "url": "/first-project/static/css/2.8aa5a7f8.chunk.css"
+  },
+  {
+    "revision": "6f927643985a48637b6a",
+    "url": "/first-project/static/css/main.118a051d.chunk.css"
+  },
+  {
+    "revision": "104055a48345a055e6c2",
+    "url": "/first-project/static/js/2.a3c95d74.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/first-project/static/js/2.a3c95d74.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6f927643985a48637b6a",
+    "url": "/first-project/static/js/main.45155426.chunk.js"
+  },
+  {
+    "revision": "3f74833601adb8630ec7",
+    "url": "/first-project/static/js/runtime-main.19d0a2a2.js"
+  }
+]);
